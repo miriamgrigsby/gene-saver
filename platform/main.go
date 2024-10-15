c7bc4ddc26af84697568272b8512114af39a4af7
Ubuntu <ubuntu@ip-172-31-41-182.us-east-2.compute.internal>
2024-10-15 23:13:01 +0000

    WIP: start on golang backend

--- platform/main.go
+++ platform/main.go
@@ -6,45 +6,41 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type genetic struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
 }
 
 func main() {
 	router := gin.Default()
 
-	// Add a home endpoint with a welcome message
-	router.GET("/", welcome)
+	// Add a home endpoint for the genetics overview homepage
+	router.GET("/", getGeneticsOverview)
+
 	// todo add readiness and liveness probes endpoints
+
 	router.GET("/health", health)
 	router.GET("/readiness", readiness)
-	router.GET("/albums", getAlbums)
 
 	router.Run("0.0.0.0:4040")
 }
 
-// welcome responds with a welcome message.
-func welcome(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "Welcome to my Go API app!"})
+// overview responds with an overview of genetics, genes, and genetic therapy data
+// TODO get genetics data, store it in db, and write adapter/handler to return that data here
+var genetics = []genetic{
+	{ID: "1", Title: "Blue Train"},
+	{ID: "2", Title: "Jeru"},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown"},
 }
+
+// getAlbums responds with the list of all albums as JSON.
+func getGeneticsOverview(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, genetics)
+}
+
 func health(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "IM ALIVE!"})
 }
 func readiness(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "IM READY!"})
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
 }
